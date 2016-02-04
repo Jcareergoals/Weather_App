0ace5438598957532a76d1d5bc59294be2973db0
Jose Chery <Jcareergoals@hotmail.com>
2016-02-04 10:16:34 -0500

    Converted kelvin to fahrenheit & included degree symbol

--- assets/weather.js
+++ assets/weather.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 		$.get("http://api.openweathermap.org/data/2.5/weather?q="+$(this).serialize()+"&appid=2294b5008c340ebe756bb0e514e7c5a3", function(res){
 			$('.1').text(res.name);
 			$('.2').text(res.weather[0].description);
-			$('.3').text(res.main.temp+"K");
+			$('.3').text(Math.floor(res.main.temp * 9 / 5 - 459.67)+"\xB0F");
 			$('.4').text(res.main.humidity+"%");
 		}, 'json');
 		return false;
