8ba5c0edd223f1beed330a2278b92b41d9dd7567
Jose Chery <Jcareergoals@hotmail.com>
2016-06-15 04:57:32 -0400

    corrected api link

--- assets/weather.js
+++ assets/weather.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('form').submit(function(){
-		$.get("http://api.openweathermap.org/data/2.5/weather?q="+$(this).serialize()+"&appid=2294b5008c340ebe756bb0e514e7c5a3", function(res){
+		$.get("https://api.openweathermap.org/data/2.5/weather?q="+$(this).serialize()+"&appid=2294b5008c340ebe756bb0e514e7c5a3", function(res){
 			$('.1').text(res.name);
 			$('.2').text(res.weather[0].description);
 			$('.3').text(Math.floor(res.main.temp * 9 / 5 - 459.67)+"\xB0F");
