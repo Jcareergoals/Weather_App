050454eebb1bcd9add065241dbb385bebf24cb20
Jose Chery <Jcareergoals@hotmail.com>
2016-08-08 02:59:10 -0400

    1. Moved site from Azure to Amazon 2. Changed placeholder text in input field 3. Changed message text 4. Added copyright message in footer

--- application/views/weather.php
+++ application/views/weather.php
@@ -30,7 +30,7 @@
 					<form action='' method='post' class='form form-horizontal'>
 						<div class="form-group">
 							<div class="col-xs-12 col-sm-4">
-								<input type='text' name='q' placeholder='Enter City or State' class='form-control'> 
+								<input type='text' name='q' placeholder='Enter your City here' class='form-control'> 
 							</div>
 							<div class="col-xs-4 col-xs-offset-4 col-sm-4 col-sm-offset-0">
 								<button type='submit' class='btn btn-info'>Get Weather</button>
@@ -45,7 +45,7 @@
 					<div class="col-xs-10 col-xs-offset-1">
 						<table class='table table-striped table-bordered col-xs-12'>
 							<tr>
-								<th class='col-xs-4'>City Name:</th><th class='col-xs-12 1'></th>
+								<th class='col-xs-4'>City:</th><th class='col-xs-12 1'></th>
 							</tr>
 							<tr>
 								<td>Description:</td><td class='2'></td>
@@ -62,12 +62,15 @@
 				<!-- Start of instructional message section -->
 				<div class="row">
 					<div class="col-xs-10 col-xs-offset-1 col-sm-9 message">
-							<p><span>Hey There! It's <a href="https://www.linkedin.com/in/jose-chery-763110a1" target="_blank">Jose</a>.</span> Thanks for visiting. Your looking at a cool and simple weather application
-							that will display real-time weather information for any city from around the world. All you have to do is enter the name of
-							any city, state, or country in the input field and click the 'Get Weather' button. You'll in return be displayed that location's
-							current weather status. If you happen to enter a state or country, you'll be displayed the weather status for that location's capital. <span>Go ahead and give it a try! </span></p>
+							<p><span>Hello there!</span> Welcome to JWeather, a place where you can literally get instant, real-time weather information for any city 
+							around the world. All you have to do is enter the city of your choice in the input field above to enjoy the experience. You can also enter a state 
+							or country to get weather information from that location’s capital.</p>
 					</div>
 				</div>
+				<!-- Copyright section -->
+                <div class="col-xs-12 copyright">
+                	<p><span>Copyright &copy; 2016 by <a href="https://www.linkedin.com/in/jose-chery-763110a1" target="_blank">Jose Chery</a></span></p>
+                </div>
 			</div>
 		</div> <!-- End Container -->
 	</body> 
